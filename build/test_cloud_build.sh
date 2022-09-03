8b1f18df3c96e49f09a0631508cf0b6261b9a1f9
Francisco Galarza <franciscogalarza@gmail.com>
2022-09-03 14:18:35 +1000

    Testing github

--- build/test_cloud_build.sh
+++ build/test_cloud_build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 repo_root=/Users/franciscogalarza/projects/francisco-com-au/core-pipeline
-PULUMI_KEY=$(cat $repo_root/pulumi-key)
+PULUMI_ACCESS_TOKEN=$(cat $repo_root/pulumi-token)
 
 docker run --rm -it \
     -v $repo_root:/app \
-    -e PULUMI_ACCESS_TOKEN=$PULUMI_KEY
-    --entrypoint /bin/sh \
+    -e PULUMI_ACCESS_TOKEN=$PULUMI_ACCESS_TOKEN \
+    --entrypoint /bin/bash \
     pulumi/pulumi
     
