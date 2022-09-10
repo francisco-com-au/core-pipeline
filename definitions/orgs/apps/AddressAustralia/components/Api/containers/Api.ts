608c798bfac374966c1267ee2a814ae426ed66eb
Francisco Galarza <franciscogalarza@gmail.com>
2022-09-10 22:37:28 +1000

    Had the apis mixed

--- definitions/orgs/apps/AddressAustralia/components/Api/containers/Api.ts
+++ definitions/orgs/apps/AddressAustralia/components/Api/containers/Api.ts
@@ -23,62 +23,12 @@ const Api: Container = {
                 name: "PORT",
                 value: "8080"
             },{
-                name: "GOOGLE_APPLICATION_CREDENTIALS",
-                value: "/var/secrets/google/key.json"
-            },{
                 name: "SOLR_PORT",
                 value: "8985"
             },{
                 name: "SOLR_PROTOCOL",
                 value: "http"
-            },{
-                name: 'MONGO_PORT',
-                value: '27017'
-            },{
-                name: "MONGO_COLLECTION",
-                secret: "website-sessions-mongo",
-                value: "db"
-            },{
-                name: "MONGO_USER_NAME",
-                secret: "website-sessions-mongo",
-                value: "username"
-            },{
-                name: "MONGO_PASSWORD",
-                secret: "website-sessions-mongo",
-                value: "password"
-            },{
-                name: "COOKIE_NAME",
-                secret: "website-sessions-mongo",
-                value: "cookie_name"
-            },{
-                name: "COOKIE_SECRET",
-                secret: "website-sessions-mongo",
-                value: "cookie_secret"
-            },{
-                name: "SENDGRID_API_KEY",
-                secret: "sendgrid-key",
-                value: "key"
-            },{
-                name: "STRIPE_SECRET_KEY",
-                secret: "stripe-key",
-                value: "secret_key"
-            },{
-                name: "STRIPE_PUBLISHABLE_KEY",
-                secret: "stripe-key",
-                value: "publishable_key"
-            },{
-                name: "STRIPE_WEBHOOK_SECRET",
-                secret: "stripe-key",
-                value: "webhook_secret"
-            },{
-                name: "MAIL_USERNAME",
-                secret: "mail-key",
-                value: "MAIL_USERNAME"
-            },{
-                name: "MAIL_PASSWORD",
-                secret: "mail-key",
-                value: "MAIL_PASSWORD"
-            },
+            }
         ]
     }
 }
